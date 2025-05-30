f83f1c561934e6d88833f749c3d8d81b1db66d94
Jake Bedford <robertjakebedford@gmail.com>
2025-05-30 08:21:50 -0600

    3rd commit

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const numbers = {
-  numberBank: [],
-  oddNumbers: [],
-  evenNumbers: [],
-};
+const appDiv = document.querySelector(`#app`);
 
-const addNumber = (Number) => {};
+appDiv.innerHTML = `
+<h1>Odds and Events</h1>
+<form>
+  <label>Add a number to the bank</label>
+  <input />
+  <button>Add number</buttton>
+  <button>Sort 1</button>
+  <button>Sort All</button>
+</form>
 
-const sortOne = () => {};
+<h2>Bank</h2>
+<output></output>
 
-const sortAll = () => {};
+<h2>Odds</h2>
+<output></output>
 
-const render = () => {};
+<h2>Evens</h2>
+<output></output>
+
+`;
+
+// const numbers = {
+//   numberBank: [],
+//   oddNumbers: [],
+//   evenNumbers: [],
+// };
+
+// const addNumber = (Number) => {};
+
+// const sortOne = () => {};
+
+// const sortAll = () => {};
+
+// const render = () => {};
